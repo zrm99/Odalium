38fdcc0533d51c1bbc4e5b0a6d0bac0dc2ef2496
zrm99 <36284384+zrm99@users.noreply.github.com>
2020-10-19 10:07:39 -0700

    Fixed lint errors.

--- Nodejs/private/misc.js
+++ Nodejs/private/misc.js
@@ -8,13 +8,15 @@ module.exports = {
   createLogFile: function createLogFile() {
     let d = new Date();
     let date = d.getMonth() + '_' + d.getDate() + '_' + d.getFullYear();
-    let fileName = date + '-log' + '.txt';
+    let fileName = date + '-log.txt';
     fs.access('logs/', fs.F_OK, function(err) {
       if (err) {
-        fs.open('logs/' + fileName, 'w', function(err) {
+        fs.open('logs/' + fileName, 'w', function() {
           if (err) {
             return console.log(err);
           }
+
+          return fs.F_OK;
         });
       }
     });
@@ -24,11 +26,13 @@ module.exports = {
   appendLogFile: function appendLogFile(fileName, fileData) {
     let d = new Date();
     let time = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + ':' + d.getMilliseconds();
-    fileData = fileData + ' (' + time + ')' + '\n';
-    fs.appendFile('logs/' + fileName, fileData, function(err) {
+    let fileDataWithTime = fileData + ` (${time})`;
+    fs.appendFile('logs/' + fileName, fileDataWithTime, function(err) {
       if (err) {
         return console.log(err);
       }
+
+      return fs.F_OK;
     });
   },
 
